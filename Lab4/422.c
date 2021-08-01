67a0271e962d0207e525a4cf389f4d2e688b4e71
Labnan <khalidmasum@iut-dhaka.edu>
2021-08-01 20:35:00 +0600

    Refactor- move output of 422 out of main, into a new function

---
+++ Lab4/422.c
@@ -0,0 +1,49 @@
+#include <pthread.h>
+#include <stdio.h>
+#include<semaphore.h>
+
+int count;
+int sequence[1000] = {0};
+
+
+int fibonacci(int n){
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
+
+
+void* buildFibonacci(void *arg){
+    for(int i = 0; i<count; i++)
+    {
+      sequence[i] = fibonacci(i);
+    }
+    return NULL;
+    
+}
+
+void outputByParent(){
+      for(int i = 0; i<count; i++){
+        printf("%d ",sequence[i]);
+    }
+}
+
+
+
+void main(){
+    pthread_t newthread;
+
+     printf("Fibonacci count?\n");
+    scanf("%d", &count);
+    pthread_create(&newthread,NULL,buildFibonacci,NULL);
+
+    
+    pthread_join(newthread,NULL);
+
+    outputByParent();
+    
+}
